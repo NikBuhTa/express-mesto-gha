b376c5b03bd7b3e70b2f8e22432a30dd0c5497f4
NikBuhTa <n.buhbinder2015@yandex.ru>
2023-08-30 13:40:50 +0300

    fix: err system task 4.1

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const { errors, celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const routes = require('./routes/index');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handleErrors = require('./middlewares/error-handler');
-const { registrationValidator } = require('./middlewares/user-validation');
+const { registrationValidator, loginValidator } = require('./middlewares/user-validation');
 
 const app = express();
 
@@ -19,12 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+app.post('/signin', loginValidator, login);
 app.post('/signup', registrationValidator, createUser);
 
 app.use(auth);
