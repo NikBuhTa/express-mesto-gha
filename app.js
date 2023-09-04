c490aacc86c0baec6e0d6df166dfc8f923c5a6d6
NikBuhTa <n.buhbinder2015@yandex.ru>
2023-09-04 11:39:23 +0300

    add: logger middleware

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handleErrors = require('./middlewares/error-handler');
 const { registrationValidator, loginValidator } = require('./middlewares/user-validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -18,12 +19,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
+app.use(requestLogger);
 
 app.post('/signin', loginValidator, login);
 app.post('/signup', registrationValidator, createUser);
 
 app.use(auth);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
